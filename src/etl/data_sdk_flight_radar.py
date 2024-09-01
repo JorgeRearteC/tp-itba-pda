eb23b8ce51cb4348b7f162754844ae7ea2c1090e
JorgeRearteC <jorgereartec@gmail.com>
2024-08-31 21:04:22 -0300

    actualizamos sdk flight radar

--- src/etl/data_sdk_flight_radar.py
+++ src/etl/data_sdk_flight_radar.py
@@ -24,25 +24,28 @@ for flight in flybondi_flights:
     flights_detail.append(flight_detail)
 
 
-df_flights_datail = pd.DataFrame(flights_detail)
+df_flights_detail = pd.DataFrame(flights_detail)
 
+print('Cantidad de vuelos:',len(df_flights_detail))
 
 ### INFO GENERAL DEL VUELO
-df_normalize_identification = pd.json_normalize(df_flights_datail['identification'])
-df_normalize_status = pd.json_normalize(df_flights_datail['status'])
-df_normalize_aircraft = pd.json_normalize(df_flights_datail['aircraft'])
-df_normalize_airline = pd.json_normalize(df_flights_datail['airline'])
-df_normalize_airport = pd.json_normalize(df_flights_datail['airport'])
-df_normalize_time = pd.json_normalize(df_flights_datail['time'])
+df_normalize_identification = pd.json_normalize(df_flights_detail['identification'])
+df_normalize_status = pd.json_normalize(df_flights_detail['status'])
+df_normalize_aircraft = pd.json_normalize(df_flights_detail['aircraft'])
+df_normalize_airline = pd.json_normalize(df_flights_detail['airline'])
+df_normalize_airport = pd.json_normalize(df_flights_detail['airport'])
+df_normalize_time = pd.json_normalize(df_flights_detail['time'])
 
 df_flights = df_normalize_identification.join(df_normalize_status).join(df_normalize_aircraft).join(df_normalize_airline).join(df_normalize_airport).join(df_normalize_time)
 
 
 
 ### RUTA DEL VUELO
-df_explode_trail = df_flights_datail.explode('trail')
+df_explode_trail = df_flights_detail.explode('trail')
 df_explode_trail = pd.DataFrame(df_explode_trail[['identification','trail']])
 df_normalize_trail = pd.json_normalize(df_explode_trail['trail'])
 df_explode_trail = df_explode_trail.join(df_normalize_trail)
 df_normalize_id = pd.json_normalize(df_explode_trail['identification'])
 df_explode_trail = df_explode_trail.join(df_normalize_id)
+
+
