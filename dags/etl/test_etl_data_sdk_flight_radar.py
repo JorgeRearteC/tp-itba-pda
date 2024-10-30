55e2449754002a7360e7fd3c816827ac004065ce
JorgeRearteC <jorgereartec@gmail.com>
2024-10-30 20:57:54 -0300

    fix test sdk

--- dags/etl/test_etl_data_sdk_flight_radar.py
+++ dags/etl/test_etl_data_sdk_flight_radar.py
@@ -1,10 +1,7 @@
 import pytest
 import pandas as pd
 from unittest.mock import MagicMock
-from etl_data_sdk_flight_radar import (
-    get_flybondi_flights,
-    get_flight_details
-)
+import etl_data_sdk_flight_radar as sdk_fr
 
 def test_get_flybondi_flights():
     # Mock de la API y su respuesta
@@ -12,7 +9,7 @@ def test_get_flybondi_flights():
     api_mock.get_flights.return_value = ["flight_1", "flight_2"]
 
     # Llamamos a la función
-    flights = get_flybondi_flights(api_mock, "FBZ", {"north": 10, "south": -10})
+    flights = sdk_fr.get_flybondi_flights(api_mock, "FBZ", {"north": 10, "south": -10})
 
     # Verificamos los resultados
     assert flights == ["flight_1", "flight_2"]
@@ -28,7 +25,7 @@ def test_get_flight_details():
 
     # Llamamos a la función
     flights = ["flight_1", "flight_2"]
-    details = get_flight_details(api_mock, flights)
+    details = sdk_fr.get_flight_details(api_mock, flights)
 
     # Verificamos los resultados
     assert details == [{"flight": "detail_1"}, {"flight": "detail_2"}]
